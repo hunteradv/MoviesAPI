ca51f19557e857a23052d0385d69d3b948c74fb4
HunterAdv <gustavopaulody@gmail.com>
2022-06-21 11:37:05 -0300

    Add project files.

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+        public void AddMovie(Movie movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+        }
+    }
+}
---
+++ MoviesAPI/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Gender { get; set; }
+        public int Duraction { get; set; }
+    }
+}
