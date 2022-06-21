1b77d1b6a2ac8e527b5a02bf71b70f38e8033bc2
HunterAdv <gustavopaulody@gmail.com>
2022-06-21 12:04:41 -0300

    Criando GetMovies para recupegar todos os filmes da lista

--- MoviesAPI/Controllers/MovieController.cs
+++ MoviesAPI/Controllers/MovieController.cs
@@ -10,17 +10,19 @@ namespace MoviesAPI.Controllers
     public class MovieController : ControllerBase
     {
         private static List<Movie> movies = new List<Movie>();
+        private static int id = 1;
 
         [HttpPost]
         public void AddMovie([FromBody]Movie movie)
         {
+            movie.Id = id++;
             movies.Add(movie);
         }
 
-        //[HttpGet]
-        //public Movie GetMovie()
-        //{
-        //    return movies.ToString;
-        //}
+        [HttpGet]
+        public IEnumerable<Movie> GetMovies()
+        {
+            return movies;
+        }
     }
 }
